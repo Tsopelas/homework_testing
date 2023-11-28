e05f636593759c4e7a3dadde22c57a145c95942b
Tsopelas <146758376+Tsopelas@users.noreply.github.com>
2023-11-29 01:19:18 +0200

    Update flawless.c

--- hw1/flawless/flawless.c
+++ hw1/flawless/flawless.c
@@ -5,28 +5,55 @@
 #define MAX_NUMBER 1000000000000
 #define MIN_NUMBER 1
 
-unsigned long long digit_separation(unsigned long long n){
+int count(unsigned long long n) {
     int count = 0;
-    long long digit_sum = 0;
-    do {
+    long long mod;
+    while (n > 0) {
+        mod = n % 10;
+        count++;
         n /= 10;
-        ++count;
-    } while (n != 0);
-    if (count % 2 == 0) {
-        while(n > 0) {
-            long long mod = n % count*10 / 2; 
-            digit_sum += mod;
-            n = n / count*10 /2;    
-        }
-        if (digit_sum ^ 2 == n) {
-            return 1;
-        }
-        
     }
+    return count;
 }
 
-int check(unsigned long long n) {
-    if (digit_separation(n)^2 == n) {
+unsigned long long digit_sum(unsigned long long n){
+    long long digit_sum = 0;
+    long long mod;
+    long long num = n;
+    int digit_count = count(n);
+    // CASE 0
+    while (num > 0) {
+        mod = num % 10;
+        digit_sum += mod;
+        num /= 10;
+    }
+    if (digit_sum * digit_sum == n) {
+        return digit_sum;
+    }
+    digit_sum = 0;
+    // CASE 1
+    for (int i = digit_count-1; i >= 1 ; i--) {
+        long long number = n;
+        long long mod = 0;
+        long long sum = 0;
+        int divisor = pow(10,i);
+        mod = number % divisor;
+        sum += mod;
+        number /= divisor;
+        sum += number ;
+        if (sum * sum == n) {
+            return sum;
+        }
+    }
+    //CASE 2
+
+    digit_sum += num;
+    return digit_sum;
+}
+
+int check (unsigned long long n) {
+    unsigned long long number = digit_sum(n);
+    if (number * number == n) {
         return 1;
     }
     return 0;
