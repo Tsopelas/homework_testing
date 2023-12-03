9e48adaf2a03e10846fad9cfb55de5918e120a81
Tsopelas <146758376+Tsopelas@users.noreply.github.com>
2023-12-03 23:45:14 +0200

    Update flawless.c

--- hw1/flawless/flawless.c
+++ hw1/flawless/flawless.c
@@ -1,101 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include <math.h>
 
-#define MAX_NUMBER 1000000000000
-#define MIN_NUMBER 1
-#define MAX_DIGITS 100
-
-long long arr[MAX_DIGITS];
-long long compositions[MAX_DIGITS];
-long long resultComposition[MAX_DIGITS];
-
-void evaluateCompositions(int n, int i, int maxN, char *strNum) {
-    if (n == 0) {
-        // Copy the successful composition to resultComposition
-        for (int k = 0; k < i; k++) {
-            resultComposition[k] = compositions[k];
-        }
-    } else if (n > 0) {
-        for (int k = 1; k <= maxN; k++) {
-            arr[i] = k;
-            int idx = i > 0 ? compositions[i - 1] : 0;
-            int digit = atoi(strNum + idx);
-            compositions[i] = digit;
-            evaluateCompositions(n - digit, i + 1, maxN, strNum);
-        }
-    }
+int calculateA(int testNumber, int divisor) {
+    return floor((float)testNumber / divisor);
 }
 
-int check(int n) {
-    char strNum[MAX_DIGITS];
-    sprintf(strNum, "%d", n);
-    int N = strlen(strNum);
+int calculateB(int testNumber, int divisor) {
+    return testNumber % divisor;
+}
 
-    double sqrtResult = sqrt(n);
-    printf("Checking if %s with square root %.1lf is an S-Number\n", strNum, sqrtResult);
-    int isSNumber = 0;
-    resultComposition[0] = (int)sqrtResult;
-
-    for (int i = 0; i < N; i++) {
-        arr[i] = 0;
-    }
-
-    evaluateCompositions(N, 0, N, strNum);
-
-    int total = 0;
-    for (int j = 0; j < N; j++) {
-        total += compositions[j];
-    }
-
-    if (total == (int)sqrtResult) {
-        isSNumber = 1;
-    }
-
-    if (isSNumber) {
-        for (int i = 0; i < N; i++) {
-            printf("%lld", resultComposition[i]);
-            if (resultComposition[i + 1] != 0) {
-                printf(" + ");
+unsigned long long recursiveNumberSplitting(unsigned long long testNumber) {
+    unsigned long long sum1 = 0;
+    unsigned long long sum2 = 0;
+    int digits = floor(log10(testNumber)) + 1;
+    
+    for (unsigned long long j = 1; j <= digits; j++) { 
+        unsigned long long divisor = pow(10, j);
+        unsigned long long b = calculateB(testNumber, divisor);
+        unsigned long long a = calculateA(testNumber, divisor);
+        int digits_a = floor(log10(a)) + 1;
+        unsigned long long division = 1;
+        unsigned long long mod;
+        printf("Iteration %llu: a=%llu, b=%llu\n", j, a, b);
+        for (int i = 1; i <= digits_a; i++) {
+            division *= 10;
+            unsigned long long temp1 = a;
+            while (temp1 > 0) {
+                mod = temp1 % division;
+                temp1 /= division;
+                sum1 = temp1 + mod + b;
+                if (sum1 * sum1 == testNumber) {
+                    printf("yes sum = %lld\n",sum1);
+                }
             }
         }
-        printf("= %.1lf\n", sqrtResult);
-        printf("%s is an S-Number\n", strNum);
-    } else {
-        printf("%s doesn't look like an S-Number\n", strNum);
+        unsigned long long temp2 = b;
+        int digits_b = floor(log10(b)) + 1;
+        unsigned long long division2 = 1;
+        unsigned long long mod2;
+        for (int i = 1; i <= digits_b; i++) {
+            division2 *= 10;
+            unsigned long long temp2 = b;
+            while (temp2 > 0) {
+                mod2 = temp2 % division2;
+                temp2 /= division2;// Default return if no matching conditions are met
+                sum2 = temp2 + mod2 + a;
+                if (sum2 * sum2 == testNumber) {
+                    printf("yes sum = %lld\n",sum2);
+                    break;
+                }
+                else {
+                    unsigned long long temp3 = temp2;
+                    while (temp3 > 0) {
+                        mod2 = temp3 % division2;
+                        temp3 /= division2;
+                        temp3 = temp2;
+                        sum2 = temp3 + temp2 + mod2 + a;
+                        if (sum2 * sum2 == testNumber) {
+                        printf("yes sum = %lld\n",sum2);
+                        break;
+                        }
+                    }
+                }
+            }
+        } 
     }
-
-    return isSNumber;
 }
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("ERROR: MORE/LESS THAN 2 INPUTS\n");
+    if (argc != 2) {
         return 1;
     }
-
-    unsigned long long low = atoll(argv[1]);
-    unsigned long long high = atoll(argv[2]);
-
-    if (low > high || low < MIN_NUMBER || high > MAX_NUMBER) {
-        printf("INPUTS NEED TO MATCH THE LIMITS\n");
-        return 1;
-    }
-
-    unsigned long long begin = sqrt(low);
-    if (begin * begin < low) {
-        ++begin;
-    }
-    long long sum = 0;
-
-    for (unsigned long long i = begin; i * i <= high; i += 2) {
-        register unsigned long long sqr = i * i;
-        if (check(sqr)) {
-            sum += sqr;
-        }
-    }
-    printf("%lld\n", sum);
+    int testNumber = atoll(argv[1]);
+    recursiveNumberSplitting(testNumber);
     return 0;
 }
