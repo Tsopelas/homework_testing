85bbc573a7c9e4782f025cf824daa92b97f25d5d
Tsopelas <146758376+Tsopelas@users.noreply.github.com>
2023-12-05 01:10:36 +0200

    Update flawless.c

--- hw1/flawless/flawless.c
+++ hw1/flawless/flawless.c
@@ -5,86 +5,81 @@
 #define MAX_NUMBER 1000000000000
 #define MIN_NUMBER 1
 
-unsigned long long calculateA(unsigned long long testNumber, unsigned long long divisor) {
+//Function to separate last digits from the number.
+unsigned long long decompute_B(unsigned long long testNumber, unsigned long long divisor) {
+    return testNumber % divisor;
+}
+//Function to separate first digits from the number.
+unsigned long long decompute_A(unsigned long long testNumber, unsigned long long divisor) {
     return floor((float)testNumber / divisor);
 }
 
-unsigned long long calculateB(unsigned long long testNumber, unsigned long long divisor) {
-    return testNumber % divisor;
-}
-
-unsigned long long breakdown(unsigned long long number,unsigned long long other_number,int divisor,unsigned long long original,unsigned long long temp) {
+//The decypher function runs the basic mod/div analyzation using recursion and returns sum to the brute force function.
+unsigned long long decypher(unsigned long long number,unsigned long long other_number,int divisor,unsigned long long original) {
     if (number <= 0) {
         return 1;
     }
-    unsigned long long mod = number % divisor;
+    unsigned long long mod;
+    mod = number % divisor;
     number /=  divisor;
     unsigned long long sum = number + mod + other_number;
-        printf("%lld %lld %lld\n", number,mod,other_number);
-    if (sum * sum == original && number != 0 && other_number != 0 && mod != 0){
+    if (sum * sum == original && mod != 0 && number != 0){
         return sum;
     }
-    int mod_digits = floor(log10(mod)) + 1;
-    unsigned long long mod2;
-    if (mod_digits > 1) {
-        temp = mod;
-        int divisor2 = 10;
-        while (temp > 0) {
-            mod2 = mod % divisor2;
-            temp /= divisor2;
-            sum = number + other_number + mod2 + temp;
-            if (sum * sum == original && number != 0 && other_number != 0 && temp != 0 && mod2 != 0){
-                return sum;
-            }
-            return 0;
-        }
+    unsigned long long mod_other = other_number % 10;
+    unsigned long long div = other_number / 10;
+    sum = number + mod + mod_other + div;
+    if (sum * sum == original && mod != 0 && number != 0){
+        return sum;
     }
-    return breakdown(number,other_number,divisor,original,temp);
+    return decypher(number,other_number,divisor,original);
 }
 
-unsigned long long recursiveNumberSplitting(unsigned long long testNumber) {
-    unsigned long long sum1 = 0;
-    unsigned long long sum2 = 0;
-    int digits = floor(log10(testNumber)) + 1;
-    for (unsigned long long j = 1; j <= digits; j++) { 
-        unsigned long long divisor = pow(10, j);
-        unsigned long long b = calculateB(testNumber, divisor);
-        unsigned long long a = calculateA(testNumber, divisor);
+//The brute force function breaks up the number into parts and calls the decypher function back and forth for each of its digit combinations in order to check all possible sums.
+unsigned long long brute_force_analyze(unsigned long long testNumber) {
+    unsigned long long summary = 0;
+    int count_digits = floor(log10(testNumber)) + 1;
+    for (unsigned long long i = 1; i <= count_digits; i++) { 
+        unsigned long long divisor = pow(10, i);
+        unsigned long long a = decompute_A(testNumber, divisor);
+        unsigned long long b = decompute_B(testNumber, divisor);
         int digits_a = floor(log10(a)) + 1;
-        sum1 = a + b;
-        if (sum1 * sum1 == testNumber) {
-            return sum1;
+        unsigned long long division = 1;
+        summary = a + b;
+        if (summary * summary == testNumber) {
+            return summary;
         }
-        int division = 1;
-        for (int i = 1; i <= digits_a; i++) {
+        for (int j = 1; j <= digits_a; j++) {
             division *= 10;
-            sum1 = breakdown(a,b,division,testNumber,a);
-            if (sum1 != 1) {
-                return sum1;
+            summary = decypher(a,b,division,testNumber);
+            if (summary != 1) {
+                return summary;
             }
         }
+        unsigned long long temp2 = b;
         int digits_b = floor(log10(b)) + 1;
-        int division2 = 1;
-        for (int i = 1; i <= digits_b; i++) {
-            division2 *= 10;
-            sum2 = breakdown(b,a,division2,testNumber,b);
-            if (sum2 * sum2 == testNumber) {
-                return sum2;
+        division = 1;
+        for (int j = 1; j <= digits_b; j++) {
+            division *= 10;
+            summary = decypher(b,a,division,testNumber);
+            if (summary != 1) {
+                return summary;
             }
         }
     }
 }
 
-
-
-int check(unsigned long long n, unsigned long long i) {
-    unsigned long long sum = recursiveNumberSplitting(n);
+//The flawless_check function checks if the sum of a numbers digits equals the square root of the number, meaning its a flawless square.
+int flawless_check(unsigned long long n, unsigned long long i) {
+    unsigned long long sum = brute_force_analyze(n); // Call brute force for number n.
+    // Check if the number is a flawless square.
     if (sum == i) {
-        return 1;
+        return 1; //Return 1 if it is.
     }
-    return 0;
+    return 0; //Return 0 if it's not.
 }
 
+// The main function is responsible for reading and calculating the basic inputs from the user.
 int main(int argc, char **argv) {
     if (argc != 3) {
         printf("ERROR: MORE/LESS THAN 2 INPUTS\n");
@@ -108,8 +103,7 @@ int main(int argc, char **argv) {
     
     for (unsigned long long i = begin; i * i <= high; i ++) {
         register unsigned long long sqr = i * i;
-        if (check(sqr,i)) {
-            printf("%lld\n",sqr);
+        if (flawless_check(sqr,i)) {
             sum += sqr;
         }
     }
