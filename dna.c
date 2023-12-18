578ede222d38cd71f41ae378907e299a0b1b8aca
Tsopelas <146758376+Tsopelas@users.noreply.github.com>
2023-12-18 22:06:56 +0200

    Update dna.c

--- dna.c
+++ dna.c
@@ -4,7 +4,47 @@
 
 #define INITIAL_CAPACITY 100
 
-char *getFileContents(char *filename, size_t *size);
+char *getFileContents(char *filename, size_t *size) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    int c;
+    size_t capacity = INITIAL_CAPACITY;
+    *size = 0;
+    char *content = (char *)malloc(capacity * sizeof(char));
+
+    if (content == NULL) {
+        perror("Error allocating memory");
+        fclose(file);
+        return NULL;
+    }
+
+    while ((c = fgetc(file)) != EOF) {
+        if (c == 'A' || c == 'C' || c == 'T' || c == 'G') {
+            content[(*size)++] = c;
+
+            if (*size == capacity) {
+                capacity *= 2;
+                content = (char *)realloc(content, capacity * sizeof(char));
+
+                if (content == NULL) {
+                    perror("Error reallocating memory");
+                    fclose(file);
+                    return NULL;
+                }
+            }
+        }
+    }
+
+    content[*size] = '\0';
+
+    fclose(file);
+
+    return content;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -25,74 +65,40 @@ int main(int argc, char *argv[]) {
         return 3;
     }
 
-    int size;
-    if (size1 < size2) {
-        size = size1;
-    } else {
-        size = size2;
+    int maxLen = 0; 
+    int endIndex = 0;  
+    int **commonLengths = (int **)malloc((size1 + 1) * sizeof(int *));
+    for (int i = 0; i <= size1; i++) {
+        commonLengths[i] = (int *)malloc((size2 + 1) * sizeof(int));
     }
-
-    int counter = 0;
-    int max = 0;
-    char max_comb[size];
-    for (int i = 1; i < size; i++){
-        if (file1Contents[i] == file2Contents[i]){
-            counter++;
-            char combination[counter];
-            combination[0] = '\0'; // Initialize the string
-            char temp[2] = {file1Contents[i], '\0'};
-            strcat(combination,temp);
-            if (counter > max){
-                max = counter;
-                printf("%s",combination);
+    for (int i = 0; i <= size1; i++){
+        for (int j = 0; j <= size2; j++) {
+            if (i == 0 || j == 0)
+                commonLengths[i][j] = 0;
+            else if (file1Contents[i - 1] == file2Contents[j - 1]) {
+                commonLengths[i][j] = commonLengths[i - 1][j - 1] + 1;
+                if (commonLengths[i][j] > maxLen) {
+                    maxLen = commonLengths[i][j];
+                    endIndex = i - 1;
+                }
+            } else {
+                commonLengths[i][j] = 0;
             }
         }
-        else{
-            counter = 0;
+    }
+
+    if (maxLen > 0) {
+        for (int i = endIndex - maxLen + 1; i <= endIndex; i++) {
+            printf("%c", file1Contents[i]);
         }
     }
+    printf ("\n");
+    for (int i = 0; i <= size1; i++) {
+        free(commonLengths[i]);
+    }
+    free(commonLengths);
     free(file1Contents);
     free(file2Contents);
 
     return 0;
-}
-
-char *getFileContents(char *filename, size_t *size) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return NULL;
-    }
-
-    int c;
-    size_t capacity = INITIAL_CAPACITY;
-    *size = 0;
-    char *content = (char *)malloc(capacity * sizeof(char));
-
-    if (content == NULL) {
-        perror("Error allocating memory");
-        fclose(file);
-        return NULL;
-    }
-
-    while ((c = fgetc(file)) != EOF) {
-        content[(*size)++] = c;
-
-        if (*size == capacity) {
-            capacity *= 2;
-            content = (char *)realloc(content, capacity * sizeof(char));
-
-            if (content == NULL) {
-                perror("Error reallocating memory");
-                fclose(file);
-                return NULL;
-            }
-        }
-    }
-
-    content[*size] = '\0';
-
-    fclose(file);
-
-    return content;
 }
